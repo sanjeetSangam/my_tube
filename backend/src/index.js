1208707bce25a687f40cf8e93f0e9b943100833c
sangamSanjeet <asanjeetsangam15@gmail.com>
2024-04-16 19:48:32 +0530

    utils code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,24 @@
-import express from "express";
 import dotenv from "dotenv";
+
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.error("app error", err);
+    });
+
+    app.listen(PORT, () => {
+      console.log("server listening on port", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("Failed to connect to database", err);
+  });
